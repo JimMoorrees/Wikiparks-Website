d9b79ec2243de7b2a7ff9f6c933ff2d1e110ded7
Jim Moorrees <jimapestaartje@gmail.com>
2017-05-30 10:41:24 +0200

    homepage

---
+++ index.php
@@ -0,0 +1,97 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>WikiParks</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<header>
+	<nav class="navbar navbar-inverse" style="border-radius: 0px; background-color: darkorange;">
+	  <div class="container-fluid">
+	    <div class="navbar-header"> 
+	      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>                        
+	      </button>
+	    </div>
+	    <div class="collapse navbar-collapse" id="myNavbar">
+	      <ul class="nav navbar-nav">
+	        <li class="active"><a style="background-color: darkorange;" href="#">Home</a></li>
+	        <li class="dropdown">
+	          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Locaties<span class="caret"></span></a>
+	          <ul class="dropdown-menu">
+	            <li><a href="#">Drenthe</a></li>
+	            <li><a href="#">Flevoland</a></li>
+	            <li><a href="#">Friesland</a></li>
+	            <li><a href="#">Gelderland</a></li>
+	            <li><a href="#">Groningen</a></li>
+	            <li><a href="#">Limburg</a></li>
+	            <li><a href="#">Noord-Brabant</a></li>
+	            <li><a href="#">Noord-Holland</a></li>
+	            <li><a href="#">Overijssel</a></li>
+	            <li><a href="#">Utrecht</a></li>
+	            <li><a href="#">Zeeland</a></li>
+	            <li><a href="#">Zuid-Holland</a></li>
+	          </ul>
+	        </li>
+	        <li><a href="#">About</a></li>
+	        <li><a href="#">FAQ</a></li>
+	      </ul>
+	      <ul class="nav navbar-nav navbar-right">
+	        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+	        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+	      </ul>
+	    </div>
+	  </div>
+	</nav>
+</header>
+    <div id="park-finder">
+      	<div id="slogan-1"><li>Pretparken vinden</li></div>  
+			<div id="slogan-2"><li>Vind alle pretparken in Nederland!</li></div>
+				<input type="submit" name="submit-button" class="submit-button" value="Zoek!">
+			</div>
+		</div>
+	</div>
+	<div id="locations">
+		<h1>Vind pretparken op deze locaties!</h1>
+	</div>
+	<div id="provincies">
+		<h1>Provincies</h1>
+		<a href=""><li>Groningen</li></a>
+		<a href=""><li>Friesland</li></a>
+		<a href=""><li>Drenthe</li></a>
+		<a href=""><li>Overijssel</li></a>
+		<a href=""><li>Flevoland</li></a>
+		<a href=""><li>Gelderland</li></a>
+		<a href=""><li>Utrecht</li></a>
+		<a href=""><li>Noord-Holland</li></a>
+		<a href=""><li>Zuid-Holland</li></a>
+		<a href=""><li>Zeeland</li></a>
+		<a href=""><li>Noord-Brabant</li></a>
+		<a href=""><li>Limburg</li></a>
+	</div>
+	<div id="hoofdsteden">
+		<h1>Steden</h1>
+		<a href=""><li>Groningen</li></a>
+		<a href=""><li>Leeuwarden</li></a>
+		<a href=""><li>Assen</li></a>
+		<a href=""><li>Zwolle</li></a>
+		<a href=""><li>Lelystad</li></a>
+		<a href=""><li>Arnhem</li></a>
+		<a href=""><li>Utrecht</li></a>
+		<a href=""><li>Haarlem</li></a>
+		<a href=""><li>Den Haag</li></a>
+		<a href=""><li>Middelburg</li></a>
+		<a href=""><li>'s-Hertogenbosch</li></a>
+		<a href=""><li>Maastricht</li></a>
+	</div>
+</body>
+</html>
+
